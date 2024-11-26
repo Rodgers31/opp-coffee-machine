b0c631092c81d4a0ffa632d364266993b980085a
Rodgers31 <otienor986@gmail.com>
2024-11-25 23:04:49 -0600

    OPP Start

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+# items
+items_menu = Menu()
+drink_names = items_menu.get_items()
+
+
+# report
+# items_report = CoffeeMaker().report()
+
+is_on = True
+while is_on:
+    choice = input(f"What would you like? ({drink_names}): ")
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        print(CoffeeMaker().report())
+    else:
+        drink_exist = items_menu.find_drink(choice)
+        if drink_exist:
+            print('menu sh', )
+
+            money_received = MoneyMachine().process_coins()
+            # MoneyMachine().make_payment()
+
+
